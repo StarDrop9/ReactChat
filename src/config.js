3b0208a82b033a1ad20c22d212098f427b473b42
StarDrop9 <kurtstarck@yahoo.com>
2019-08-02 19:09:11 -0500

    Z commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/67c95eae-583a-4234-8b3d-54aae574011b/token";
+const instanceLocator = "v1:us1:67c95eae-583a-4234-8b3d-54aae574011b";
+//updated to mine
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
+
